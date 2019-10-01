ecc0f48fc531cce9506e567d88e8f548d6732b19
Zac Higgins <zachiggins@Higgins-iMac.local>
2019-10-01 16:12:15 -0600

    added grey background and scale up on nav links when mouse hovers over them

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
 // Your code goes here
+const navLinks = document.querySelectorAll('.nav-link');
+
+//grey background and scale-up on nav link hover
+navLinks.forEach((link) => {
+    link.addEventListener('mouseenter', () => {
+       link.style.transform = "scale(1.2)";
+       link.style.transition = "all 0.3s"
+       link.style.backgroundColor = '#dfe4ed'
+    })
+    link.addEventListener('mouseleave', () => {
+        link.style.transform = "scale(1)";
+        link.style.transition = "all 0.3s"
+        link.style.background = 'none'
+    })
+    link.addEventListener
+});
+
+//random background color on mouse double click
+function random_bg_color() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")"
+    return bgColor;
+}
+const crazyMouse = document.querySelector('body');
+window.addEventListener('dblclick', () => {
+    crazyMouse.style.backgroundColor = random_bg_color();
+});
+
